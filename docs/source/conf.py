acc7f699e08a842134a64ac623e0093478afd981
wenzhang.liu <liu_wzh@foxmail.com>
2023-05-31 20:54:54 +0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,8 +24,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'renku'
-html_static_path = ['_static']
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The master toctree document.
 master_doc = 'index'
