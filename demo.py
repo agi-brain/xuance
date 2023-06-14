857cbad2b8145e1b829757bf0231875813763c05
wenzhang.liu <liu_wzh@foxmail.com>
2023-06-14 09:39:42 +0800

    dqn atari

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ from xuanpolicy import get_runner
 
 def parse_args():
     parser = argparse.ArgumentParser("Multi-Agent Reinforcement Learning With Causality Detection.")
-    parser.add_argument("--method", type=str, default="a2c")
+    parser.add_argument("--method", type=str, default="dqn")
     parser.add_argument("--env", type=str, default="atari")
-    parser.add_argument("--env-id", type=str, default="ALE/Pong-v5")
+    parser.add_argument("--env-id", type=str, default="ALE/Breakout-v5")
     parser.add_argument("--test", type=int, default=0)
     parser.add_argument("--device", type=str, default="cuda:0")
     return parser.parse_args()
