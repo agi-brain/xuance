4be584057d2c9b5cef47e5e4df56b8531c05c74a
wenzhang.liu <liu_wzh@foxmail.com>
2023-06-20 10:18:11 +0800

    dqn box2d

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from xuanpolicy import get_runner
 
 def parse_args():
     parser = argparse.ArgumentParser("Multi-Agent Reinforcement Learning With Causality Detection.")
-    parser.add_argument("--method", type=str, default="dqn")
+    parser.add_argument("--method", type=str, default="drqn")
     parser.add_argument("--env", type=str, default="classic_control")
     parser.add_argument("--env-id", type=str, default="CartPole-v1")
     parser.add_argument("--test", type=int, default=0)
