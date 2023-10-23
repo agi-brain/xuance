7e161dba9285beed9e4efaf3f2051ba748c99f82
wenzhangliu <liu_wzh@foxmail.com>
2023-10-23 11:34:42 +0800

    logo

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ from xuance import get_runner
 def parse_args():
     parser = argparse.ArgumentParser("Run a demo.")
     parser.add_argument("--method", type=str, default="a2c")
-    parser.add_argument("--env", type=str, default="atari")
-    parser.add_argument("--env-id", type=str, default="ALE/Pong-v5")
+    parser.add_argument("--env", type=str, default="classic_control")
+    parser.add_argument("--env-id", type=str, default="CartPole-v1")
     parser.add_argument("--test", type=int, default=0)
     parser.add_argument("--device", type=str, default="cuda:0")
     return parser.parse_args()
