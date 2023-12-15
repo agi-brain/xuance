3bdaf949234acc870a889c70be12e40d0191d2e3
wenzhangliu <liu_wzh@foxmail.com>
2023-12-16 00:29:51 +0800

    update to version 1.0.5

--- xuance/__init__.py
+++ xuance/__init__.py
@@ -2,4 +2,4 @@ from xuance import common, environment, configs
 from xuance import tensorflow, mindspore, torch
 from xuance.common.common_tools import get_runner, get_arguments
 
-__version__ = '1.0.2'
+__version__ = '1.0.5'
