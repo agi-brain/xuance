310c699784c7eb7a39e406211ca544c25f0274c8
wenzhangliu <liu_wzh@foxmail.com>
2024-02-21 18:26:41 +0800

    examples for new envs for MARL

--- xuance/torch/runners/runner_marl.py
+++ xuance/torch/runners/runner_marl.py
@@ -1,5 +1,5 @@
 """
-This is demo of runner for multi-agent reinforcement learning.
+This is demo of runner for cooperative multi-agent reinforcement learning.
 """
 from .runner_basic import Runner_Base
 
@@ -7,4 +7,5 @@ from .runner_basic import Runner_Base
 class Runner_MARL(Runner_Base):
     def __init__(self, args):
         super(Runner_MARL, self).__init__(args)
+
 
