86b235de6e6e884b0e7cc64342bf53f5352dbde8
wenzhangliu <liu_wzh@foxmail.com>
2024-06-02 18:15:56 +0800

    APIs: common

--- xuance/common/__init__.py
+++ xuance/common/__init__.py
@@ -1,14 +1,12 @@
-from xuance.common.common_tools import (EPS, recursive_dict_update, get_config, get_arguments, get_runner,
-                                        create_directory, combined_shape, space2shape, discount_cumsum, get_time_string)
+from xuance.common.common_tools import EPS, recursive_dict_update, get_config, get_arguments, get_runner,\
+    create_directory, combined_shape, space2shape, discount_cumsum, get_time_string
 from xuance.common.statistic_tools import mpi_mean, mpi_moments, RunningMeanStd
-from xuance.common.memory_tools import (create_memory, store_element, sample_batch, Buffer, EpisodeBuffer,
-                                        DummyOnPolicyBuffer, DummyOnPolicyBuffer_Atari,
-                                        DummyOffPolicyBuffer, DummyOffPolicyBuffer_Atari,
-                                        RecurrentOffPolicyBuffer, PerOffPolicyBuffer)
-from xuance.common.memory_tools_marl import (BaseBuffer, MARL_OnPolicyBuffer, MARL_OnPolicyBuffer_RNN,
-                                             MeanField_OnPolicyBuffer, MeanField_OffPolicyBuffer,
-                                             COMA_Buffer, COMA_Buffer_RNN,
-                                             MARL_OffPolicyBuffer, MARL_OffPolicyBuffer_RNN)
+from xuance.common.memory_tools import create_memory, store_element, sample_batch, Buffer, EpisodeBuffer, \
+    DummyOnPolicyBuffer, DummyOnPolicyBuffer_Atari, DummyOffPolicyBuffer, DummyOffPolicyBuffer_Atari, \
+    RecurrentOffPolicyBuffer, PerOffPolicyBuffer
+from xuance.common.memory_tools_marl import BaseBuffer, MARL_OnPolicyBuffer, MARL_OnPolicyBuffer_RNN, \
+    MeanField_OnPolicyBuffer, MeanField_OffPolicyBuffer, COMA_Buffer, COMA_Buffer_RNN, \
+    MARL_OffPolicyBuffer, MARL_OffPolicyBuffer_RNN
 from xuance.common.segtree_tool import SegmentTree, SumSegmentTree, MinSegmentTree
 
 __all__ = [
