aedc9d7f0e68f81870e468bd0cc18eabad6b1957
wenzhangliu <liu_wzh@foxmail.com>
2024-12-11 19:20:33 +0800

    update masac

--- xuance/torch/agents/multi_agent_rl/masac_agents.py
+++ xuance/torch/agents/multi_agent_rl/masac_agents.py
@@ -50,6 +50,7 @@ class MASAC_Agents(ISAC_Agents):
                 device=device, use_distributed_training=self.distributed_training,
                 use_parameter_sharing=self.use_parameter_sharing, model_keys=self.model_keys,
                 use_rnn=self.use_rnn, rnn=self.config.rnn if self.use_rnn else None)
+            self.continuous_control = True
         else:
             raise AttributeError(f"{agent} currently does not support the policy named {self.config.policy}.")
 
