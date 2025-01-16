798b0758fb84ad747653cdfe2947e0a8566f58b2
wenzhangliu <liu_wzh@foxmail.com>
2025-01-16 22:40:45 +0800

    example for dqn tuning

--- examples/hyperparameter_tuning/tune_dqn.py
+++ examples/hyperparameter_tuning/tune_dqn.py
@@ -1,4 +1,5 @@
 from xuance.common import HyperParameterTuner, set_hyperparameters
+from optuna.visualization import plot_optimization_history
 
 tuner = HyperParameterTuner(method='dqn',
                             config_path='../dqn/dqn_configs/dqn_cartpole.yaml',
@@ -8,8 +9,11 @@ tuner = HyperParameterTuner(method='dqn',
 selected_hyperparameters = tuner.select_hyperparameter(['learning_rate'])
 
 overrides = {
-    'learning_rate': [0.001, 0.0001, 0.00001]
+    'learning_rate': [0.001, 0.0001, 0.00001],  # Categorical: Search in the set of {0.001, 0.0001, 0.00001}.
+    'gamma': (0.9, 0.99)  # Search from 0.9 to 0.99.
 }
 selected_hyperparameters = set_hyperparameters(selected_hyperparameters, overrides)
 
-tuner.tune(selected_hyperparameters, n_trials=5, pruner=None)
+study = tuner.tune(selected_hyperparameters, n_trials=30, pruner=None)
+fig = plot_optimization_history(study)
+fig.show()
