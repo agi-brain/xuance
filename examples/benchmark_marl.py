83fe2f662d6b82080b02d6ea6e5b91d881108047
wenzhangliu <liu_wzh@foxmail.com>
2025-01-18 13:51:13 +0800

    remove to examples

---
+++ examples/benchmark_marl.py
@@ -0,0 +1,29 @@
+import argparse
+from xuance import get_runner
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Run benchmark results for MARL.")
+    parser.add_argument("--method", type=str, default="maddpg")
+    parser.add_argument("--env", type=str, default="mpe")
+    parser.add_argument("--env-id", type=str, default="simple_spread_v3")
+    parser.add_argument("--test", type=int, default=0)
+    parser.add_argument("--device", type=str, default="cuda:0")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    parser = parse_args()
+    ########################################################################
+    # You can also modify the arguments not listed above here. For example:
+    # parser.parallels = 1
+    # parser.running_steps = 1000000
+    # ...
+    ########################################################################
+    runner = get_runner(method=parser.method,
+                        env=parser.env,
+                        env_id=parser.env_id,
+                        parser_args=parser,
+                        is_test=parser.test)
+    runner.benchmark()
