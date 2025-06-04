d36556dd1f584ddfbd6897b82f500fa18f2895ec
wenzhangliu <liu_wzh@foxmail.com>
2025-06-04 16:15:56 +0800

    add callback hooks in agents

--- tests/torch/test_torch_marl_based.py
+++ tests/torch/test_torch_marl_based.py
@@ -95,12 +95,12 @@ class TestValueBaseAlgo(unittest.TestCase):
         runner = get_runner(method="commnet", env='mpe', env_id='simple_spread_v3', parser_args=args)
         runner.run()
 
-    def test_ic3net(self):
+    def test_commnet(self):
         args = Namespace(dl_toolbox='torch', device=device, running_steps=n_steps, test_mode=test_mode)
         runner = get_runner(method="ic3net", env='mpe', env_id='simple_spread_v3', parser_args=args)
         runner.run()
 
-    def test_ic3net(self):
+    def test_tarmac(self):
         args = Namespace(dl_toolbox='torch', device=device, running_steps=n_steps, test_mode=test_mode)
         runner = get_runner(method="tarmac", env='mpe', env_id='simple_spread_v3', parser_args=args)
         runner.run()
