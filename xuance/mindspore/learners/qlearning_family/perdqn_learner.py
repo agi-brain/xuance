741580c8f405496e847282625cbc6d95aa634904
wenzhangliu <liu_wzh@foxmail.com>
2025-08-11 00:15:12 +0800

    update: perdqn for mindspore

--- xuance/mindspore/learners/qlearning_family/perdqn_learner.py
+++ xuance/mindspore/learners/qlearning_family/perdqn_learner.py
@@ -4,11 +4,10 @@ Paper link: https://arxiv.org/pdf/1511.05952.pdf
 Implementation: MindSpore
 """
 import numpy as np
-from xuance.mindspore import ms, Module, Tensor, optim
-from xuance.mindspore.learners import Learner
 from argparse import Namespace
-from mindspore.ops import OneHot
-from mindspore.nn import MSELoss
+from mindspore import nn
+from xuance.mindspore import ms, ops, Module, Tensor, optim
+from xuance.mindspore.learners import Learner
 
 
 class PerDQN_Learner(Learner):
@@ -21,38 +20,36 @@ class PerDQN_Learner(Learner):
                                                      total_iters=self.config.running_steps)
         self.gamma = config.gamma
         self.sync_frequency = config.sync_frequency
-        self.mse_loss = MSELoss()
-        self.one_hot = OneHot()
+        self.mse_loss = nn.MSELoss()
+        self.gather = ops.Gather(batch_dims=-1)
         self.n_actions = self.policy.action_dim
         # Get gradient function
         self.grad_fn = ms.value_and_grad(self.forward_fn, None, self.optimizer.parameters, has_aux=True)
         self.policy.set_train()
 
-    def forward_fn(self, x, a, label):
-        _, _, _evalQ = self.policy(x)
-        _predict_Q = (_evalQ * self.one_hot(a.astype(ms.int32), _evalQ.shape[1], Tensor(1.0), Tensor(0.0))).sum(axis=-1)
-        loss = self.mse_loss(_predict_Q, label)
-        return loss, _predict_Q
-
-    def update(self, **samples):
-        self.iterations += 1
-        obs_batch = Tensor(samples['obs'])
-        act_batch = Tensor(samples['actions'])
-        rew_batch = Tensor(samples['rewards'])
-        next_batch = Tensor(samples['obs_next'])
-        ter_batch = Tensor(samples['terminals'])
-
+    def forward_fn(self, obs_batch, act_batch, next_batch, rew_batch, ter_batch):
+        _, _, evalQ = self.policy(obs_batch)
         _, _, targetQ = self.policy.target(next_batch)
         targetQ = targetQ.max(axis=-1)
         targetQ = rew_batch + self.gamma * (1 - ter_batch) * targetQ
 
-        _, _, evalQ = self.policy(obs_batch)
-        predict_Q = (evalQ * self.one_hot(act_batch.astype(ms.int32), evalQ.shape[1],
-                                          Tensor(1.0), Tensor(0.0))).sum(axis=-1)
-        td_error = targetQ - predict_Q
+        predict_Q = self.gather(evalQ, act_batch, axis=-1).reshape(-1)
+        loss = self.mse_loss(logits=predict_Q, labels=ops.stop_gradient(targetQ))
 
-        (loss, predictQ), grads = self.grad_fn(obs_batch, act_batch, targetQ)
+        return loss, predict_Q, targetQ
+
+    def update(self, **samples):
+        self.iterations += 1
+        obs_batch = Tensor(samples['obs'])
+        act_batch = Tensor(samples['actions'].reshape(-1, 1), dtype=ms.int32)
+        rew_batch = Tensor(samples['rewards'])
+        next_batch = Tensor(samples['obs_next'])
+        ter_batch = Tensor(samples['terminals'])
+
+        (loss, predictQ, targetQ), grads = self.grad_fn(obs_batch, act_batch, next_batch, rew_batch, ter_batch)
         self.optimizer(grads)
+
+        td_error = ops.stop_gradient(targetQ - predictQ)
 
         # hard update for target network
         if self.iterations % self.sync_frequency == 0:
