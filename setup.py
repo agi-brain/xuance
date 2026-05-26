6348cb0b53eb55eed02c9d1ae0b99286958d12cb
wenzhangliu <liu_wzh@foxmail.com>
2026-05-27 00:44:18 +0800

    release: v1.4.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                 "environment/magent2/libmagent.dylib"  # for magent2 environment on macOS (for Intel CPU)
             ]
     },
-    version="1.4.2",
+    version="1.4.3",
     description='XuanCe: A Comprehensive and Unified Deep Reinforcement Learning Library.',
     long_description='XuanCe is an open-source ensemble of Deep Reinforcement Learning (DRL) algorithm implementations. We call it as Xuan-Ce (玄策) in Chinese. "Xuan (玄)" means incredible and magic box, "Ce (策)" means policy. DRL algorithms are sensitive to hyperparameters tuning, varying in performance with different tricks, and suffering from unstable training processes, therefore, sometimes DRL algorithms seems elusive and "Xuan". This project gives a thorough, high-quality and easy-to-understand implementation of DRL algorithms, and hope this implementation can give a hint on the magics of reinforcement learning. We expect it to be compatible with multiple deep learning backends (PyTorch, TensorFlow, and MindSpore), and hope it can really become a zoo full of DRL algorithms.',
     author='Wenzhang Liu, et al.',
@@ -46,8 +46,11 @@ setup(
                       "protobuf==3.20.0"],
         "all": [
             "torch",
-            "tensorflow==2.6.0",
-            "mindspore==2.2.14"  # mindspore might be installed manually.
+            "torchvision",
+            "tensorflow==2.19.0",
+            "tf_keras==2.19.0",
+            "mindspore==2.6.0",  # mindspore might be installed manually.
+            "protobuf==3.20.0",
         ],
         "tune": [
             "optuna>=4.1.0",
@@ -66,10 +69,10 @@ setup(
         "einops": ["einops==0.8.1"],  # default version is 0.8.1 for ViT
     },
     install_requires=[
-        "numpy",  # suggest version: >=1.21.6
+        "numpy>=1.21.0, <2.0.0",  # suggest version: >=1.21.6
         "scipy",  # suggest version: >=1.15.3
         "PyYAML",  # suggest version: 6.0
-        "gymnasium",  # suggest version: >=1.1.1
+        "gymnasium>=0.28.0, <1.3.0",  # suggest version: >=1.1.1
         "pygame",  # suggest version: >=2.1.0
         "tqdm",  # suggest version: >=4.66.3
         "pyglet==1.5.15",  # suggest version: ==1.5.15
@@ -79,7 +82,7 @@ setup(
         "moviepy==1.0.3",  # suggest version: >=1.0.3
         "imageio",  # suggest version: 2.9.0
         "opencv-python",  # suggest version: 4.5.4.58
-        "torch",
+        "torch>=2.0.0, <3.0.0",
         "torchvision"
     ],
     setup_requires=['pytest-runner'],
